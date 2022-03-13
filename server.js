4cb2b7602ad53cd273add490ba076ad6fcba0f8f
Sergey Shevtsov <Sergeyprostoas@gmail.com>
2022-03-13 23:24:59 +0300

    feat: Chapter 17 / Lessons 1-19 complete
    Testing GraphQL

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { loadFilesSync } = require('@graphql-tools/load-files');
+
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
+
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: resolversArray,
+});
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  }),
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
